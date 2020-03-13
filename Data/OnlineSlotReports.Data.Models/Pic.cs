e007fcec5f6d001920cd56c72d783cf94de8c7e5
Bogdan Chulakov <49172210+BogdanChulakov@users.noreply.github.com>
2020-03-13 02:11:34 +0200

    increase Pic Model

---
+++ Data/OnlineSlotReports.Data.Models/Pic.cs
@@ -0,0 +1,21 @@
+﻿using OnlineSlotReports.Data.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineSlotReports.Data.Models
+{
+    public class Pic : BaseDeletableModel<string>
+    {
+        public Pic()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Url { get; set; }
+
+        public string GamingHallId { get; set; }
+
+        public virtual GamingHall GamingHall { get; set; }
+    }
+}
